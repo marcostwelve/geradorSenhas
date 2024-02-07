4b6b65d81a6fae9909837536ba885e930d874448
marcostwelve <marcostwelve>
2024-02-07 18:29:50 -0300

    Gerador Senha v1

---
+++ js/script.js
@@ -0,0 +1,54 @@
+// Seleção de elementos
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatePasswordElement = document.querySelector("#generated-password");
+
+
+//Funções
+// letras, números e símbolos
+const getLetterLowerCase = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+
+const getterLetterUpperCase = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+const getNumber = () => {
+  return Math.floor(Math.random() * 10).toString();
+};
+
+const getSymbol = () => {
+  const symbols = "(){}[]=<>/,.!@#$%&*+-";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+};
+
+const generatePassword = (getLetterLowerCase, getterLetterUpperCase, getNumber, getSymbol) => {
+  let password = "";
+
+  const passwordLength = 10;
+
+  const generators = [
+    getLetterLowerCase,
+    getterLetterUpperCase,
+    getNumber,
+    getSymbol,
+  ];
+
+  for(i = 0; i < passwordLength; i = i + generators.length) {
+    generators.forEach(() => {
+      const randomValue = generators[Math.floor(Math.random() * generators.length)]();
+
+      password += randomValue;
+    });
+  }
+
+  password = password.slice(0, passwordLength);
+  generatePasswordElement.style.display = "block";
+  generatePasswordElement.querySelector("h4").innerText = password;
+};
+
+
+//Eventos
+generatePasswordButton.addEventListener("click", () => {
+  generatePassword(getLetterLowerCase, getterLetterUpperCase, getNumber, getSymbol);
+});
